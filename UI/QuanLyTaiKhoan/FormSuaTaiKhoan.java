4de121c92691cbc9a1cbd854fce48e287ac869e9
intansemc2 <intansemc2@gmail.com>
2020-06-04 12:38:11 +0700

    Thêm tooltip cho nút

--- UI/QuanLyTaiKhoan/FormSuaTaiKhoan.java
+++ UI/QuanLyTaiKhoan/FormSuaTaiKhoan.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package UI.QuanLyNhanVien;
+package UI.QuanLyTaiKhoan;
 
 import BUS.TaiKhoanBUS;
 import DTO.TaiKhoanValidateException;
